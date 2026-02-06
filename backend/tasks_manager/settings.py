3fc3df04c61a0adb724a25a6c31f0c5059c53d77
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-02-06 12:41:48 +0300

    projects containerization

--- backend/tasks_manager/settings.py
+++ backend/tasks_manager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=n+z+((w*sa(wcfi!#v+5l96jg=r7bc@2d%a_weupas*-fe_mu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["api.urbantrends.dev", "127.0.0.1", "localhost", "149.102.132.191", "www.urbantrends.dev", "urbantrends.dev"]
 
 
 # Application definition
@@ -74,11 +74,17 @@ WSGI_APPLICATION = 'tasks_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
