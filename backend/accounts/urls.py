263f941ad9fc3020af478b68ddad9455840133ab
Top G Dev <developer@vmi3029671.contaboserver.net>
2026-02-13 18:53:22 +0300

    token refresh

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import hello, createuser, login
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 # relevant patterns
 urlpatterns = [
     path('greetings/', hello, name="greetings"),
     path('create/', createuser, name='create user'),
     path('login/', login, name='Login user'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
